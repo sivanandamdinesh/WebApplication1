b0273f3e7b4528e92682170e0ccb7b5ff150bbf1
sivan <sivanandamdinesh@gmail.com>
2020-11-17 12:52:23 +0530

    Add project files.

---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml("<item><name>wrench</name></item>");
+            //Save the document to a file.
+            doc.Save(@"D:\Dinesh\TestApp\test.xml");
+        }
+    }
+}
